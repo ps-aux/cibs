cd7326b801c1651d7a2f3739ab4f678372ae8629
psaux <seriouz.accz@gmail.com>
2020-03-11 13:23:18 +0100

    Fic tests?

--- test/docker/docker.spec.ts
+++
@@ -1,7 +0,0 @@
-import { buildDockerImage } from 'src/docker/buildDockerImage'
-
-describe('docker', () => {
-    it('buildDockerImage', () => {
-        buildDockerImage('foo', '123', __dirname)
-    })
-})
