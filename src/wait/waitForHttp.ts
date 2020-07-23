858a3aa6ddc548484b2cb2256bb681fe5af6f168
psaux <seriouz.accz@gmail.com>
2020-07-23 15:56:58 +0200

    Fix back timeout

--- src/wait/waitForHttp.ts
+++ src/wait/waitForHttp.ts
@@ -3,5 +3,6 @@ import waitOn from 'wait-on'
 
 export const waitForHttp = async (url: string, timeout = 3000) =>
     waitOn({
-        resources: [url]
+        resources: [url],
+        timeout
     })
