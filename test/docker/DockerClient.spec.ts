ce72116f0ee12192fc3ac01fd5c35d627e3dbba7
psaux <seriouz.accz@gmail.com>
2021-03-19 21:28:29 +0100

    Fix TS

--- test/docker/DockerClient.spec.ts
+++ test/docker/DockerClient.spec.ts
@@ -1,6 +1,7 @@
 import { DockerClient } from 'src/docker/DockerClient'
 import { LocalShellCmdExecutor } from 'src/util/shell/LocalShellCmdExecutor'
 import { ConsoleLogger } from 'src/log/ConsoleLogger'
+import { FileSystem } from '../../src/fs/FileSystem'
 
 const registryApiUrl = 'https://index.docker.io/v1/'
 const registryName = 'yijohor934'
@@ -12,7 +13,11 @@ describe.skip('DockerClient', () => {
     const imageName = 'cibs-test-image'
 
     const l = new ConsoleLogger()
-    const sut = new DockerClient(new LocalShellCmdExecutor(l), l)
+    const sut = new DockerClient(
+        new LocalShellCmdExecutor(l),
+        new FileSystem(),
+        l
+    )
 
     it('works', () => {
         const name = registryName + '/' + imageName
